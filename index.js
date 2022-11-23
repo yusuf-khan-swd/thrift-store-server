9ba4091a8410857796119d01656dd5e85fc7c917
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-24 01:27:06 +0600

    basic server running

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Thrift store server is running");
+});
+
+app.listen(port, () => {
+  console.log(`Thrift store server is running on port ${port}`);
+});
