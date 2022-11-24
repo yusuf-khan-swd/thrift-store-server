ad54181df2f541d5b842e170d425cd394a43adba
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-24 23:58:08 +0600

    create a token

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -27,6 +28,12 @@ async function run() {
       const query = { userEmail: email };
       const result = await usersCollection.findOne(query);
       res.send({ result });
+    });
+
+    app.get("/jwt", async (req, res) => {
+      const email = req.query.email;
+      const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1d' })
+      res.send({ token });
     });
 
   }
