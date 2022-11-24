227b3a4babdc6e03661b82d2f43cb79f2ccee978
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-24 19:27:58 +0600

    add a user to db

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
@@ -6,6 +7,28 @@ const port = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tjl9nwy.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+  try {
+    const usersCollection = client.db("thriftStore").collection("users");
+
+    app.post("/user", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+  }
+  finally {
+
+  }
+};
+
+run().catch(console.log);
 
 app.get("/", (req, res) => {
   res.send("Thrift store server is running");
