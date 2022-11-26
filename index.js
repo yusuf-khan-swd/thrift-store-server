8b85bfa637f6bc765e2a9b17c1d459cfd81ec84c
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-26 20:02:02 +0600

    delete a seller

--- index.js
+++ index.js
@@ -164,6 +164,24 @@ async function run() {
       res.send(result);
     });
 
+    // app.put("/all-sellers/:id", async(req, res) => {
+    //   const id = req.params.id;
+    //   const filter = {_id: ObjectId(id)};
+    //   const options = {upsert: true};
+    //   const updatedDoc = {
+    //     $set: {
+
+    //     }
+    //   }
+    // })
+
+    app.delete("/all-sellers/:id", verifyJWT, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const result = await usersCollection.deleteOne(filter);
+      res.send(result);
+    });
+
   }
   finally {
 
