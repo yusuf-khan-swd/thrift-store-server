9aad6ec59852e1af239c60210d5fecb7ebeeabb6
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-28 01:29:36 +0600

    delete a product

--- index.js
+++ index.js
@@ -262,6 +262,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/products/:id", verifyJWT, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productsCollection.deleteOne(query);
+      res.send(result);
+    });
+
   }
   finally {
 
